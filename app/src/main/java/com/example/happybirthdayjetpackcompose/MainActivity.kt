94f6cf0d727335073057a6e9b6d7025dae1b0fe0
Amadeusz Lisiecki <amadeusz.lisiecki.1993@gmail.com>
2022-08-30 21:31:09 +0200

    Ustawienie i skalowanie obrazu do komponowania.

--- app/src/main/java/com/example/happybirthdayjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/happybirthdayjetpackcompose/MainActivity.kt
@@ -3,14 +3,15 @@ package com.example.happybirthdayjetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import com.example.happybirthdayjetpackcompose.ui.theme.HappyBirthdayJetpackComposeTheme
@@ -22,7 +23,7 @@ class MainActivity : ComponentActivity() {
             HappyBirthdayJetpackComposeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    BirthdayGreetingWithText("Wszystkiego najlepszego Maciek!", "- od Amadeusza")
+                    BirthdayGreetingWithImage("Wszystkiego najlepszego Maciek!", "- od Amadeusza")
                 }
             }
         }
@@ -43,10 +44,26 @@ fun BirthdayGreetingWithText(message: String, from: String) {
     }
 }
 
+@Composable
+fun BirthdayGreetingWithImage(message: String, from: String) {
+    val image = painterResource(R.drawable.androidparty)
+    Box {
+        Image(
+            painter = image,
+            contentDescription = null,
+            modifier = Modifier
+                .fillMaxHeight()
+                .fillMaxWidth(),
+            contentScale = ContentScale.Crop
+        )
+    }
+    BirthdayGreetingWithText(message = message, from = from)
+}
+
 @Preview(showBackground = true)
 @Composable
 fun BirthDayCardPreview() {
     HappyBirthdayJetpackComposeTheme() {
-        BirthdayGreetingWithText("Wszystkiego najlepszego Maciek!", "- od Amadeusza")
+        BirthdayGreetingWithImage("Wszystkiego najlepszego Maciek!", "- od Amadeusza")
     }
 }
